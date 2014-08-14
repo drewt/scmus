56b3e00188b277006cd3e9387cfb8966f2fd7a10
Drew Thoreson <drew.thoreson@alumni.ubc.ca>
2014-08-14 14:00:06 -0700

    commane-line: history
    The up and down keys cycle through previously entered commands/queries
    in eval/search mode.  Implementation uses a hand-rolled doubly-linked
    list with a cursor.

--- command-line.scm
+++ command-line.scm
@@ -15,10 +15,104 @@
 ;; along with this program; if not, see <http://www.gnu.org/licenses/>.
 ;;
 
-(declare (unit command-line)
-         (uses editable eval-mode ui-curses))
-
 (require-extension ncurses)
+
+(declare (unit command-line)
+         (uses editable eval-mode ui-curses)
+         (export command-line-text-set! command-line-mode command-line-text
+                 enter-eval-mode enter-search-mode))
+
+;; history {{{
+
+(define *eval-history* '())
+(define *search-history* '())
+
+(define (iter-data iter)
+  (caar iter))
+
+(define (iter-next iter)
+  (cdr iter))
+
+(define (iter-prev iter)
+  (cdar iter))
+
+(define (iter-move-next iter)
+  (if (null? (iter-next iter))
+    iter
+    (iter-next iter)))
+
+(define (iter-move-prev iter)
+  (if (null? (iter-prev iter))
+    iter
+    (iter-prev iter)))
+
+(define (iter-reset iter)
+  (if (or (null? iter) (null? (iter-prev iter)))
+    iter
+    (iter-reset (iter-prev iter))))
+
+(define (iter-add! iter elm)
+  (let* ((head (iter-reset iter))
+         (new (cons (cons elm '()) head)))
+    (unless (null? head)
+      (set-cdr! (car head) new))
+    new))
+
+(define (iter-pop! iter)
+  (let* ((head (iter-reset iter))
+         (next (iter-next head)))
+    (if (null? next)
+      '()
+      (begin
+        (set-cdr! (car next) '())
+        next))))
+
+(define (iter-set! iter elm)
+  (set-car! (car iter) elm))
+
+(define (iter-init! iter)
+  (iter-add! (iter-reset iter) ""))
+
+(define (eval-mode?)
+  (eqv? *command-line-mode* 'eval-mode))
+
+(define (history)
+  (if (eval-mode?)
+    *eval-history*
+    *search-history*))
+
+(define (history-init!)
+  (if (eval-mode?)
+    (set! *eval-history* (iter-init! *eval-history*))
+    (set! *search-history* (iter-init! *search-history*))))
+
+(define (history-abort!)
+  (if (eval-mode?)
+    (set! *eval-history* (iter-pop! *eval-history*))
+    (set! *search-history* (iter-pop! *search-history*))))
+
+(define (history-next!)
+  (if (eval-mode?)
+    (set! *eval-history* (iter-move-next *eval-history*))
+    (set! *search-history* (iter-move-next *search-history*))))
+
+(define (history-prev!)
+  (if (eval-mode?)
+    (set! *eval-history* (iter-move-prev *eval-history*))
+    (set! *search-history* (iter-move-prev *search-history*))))
+
+(define (history-add! elm)
+  (if (string=? elm "")
+    (history-abort!)
+    (iter-set! (iter-reset (history)) elm)))
+
+(define (history-data)
+  (iter-data (history)))
+
+(define (history-set! elm)
+  (iter-set! (history) elm))
+
+;; history }}}
 
 (define (command-line-changed!)
   (register-event! 'command-line-changed))
@@ -44,6 +138,7 @@
   (editable-length *command-line*))
 
 (define (command-line-leave editable)
+  (history-add! (editable-text editable))
   (editable-clear! editable)
   (set! *command-line-mode* 'normal-mode)
   (set-input-mode! 'normal-mode))
@@ -74,8 +169,12 @@
   (key-case key
     ((KEY_ENTER)
       (command-line-commit! editable))
-    ((KEY_UP) (void)) ; TODO: history
-    ((KEY_DOWN) (void))
+    ((KEY_UP)
+      (history-next!)
+      (editable-text-set! editable (history-data)))
+    ((KEY_DOWN)
+      (history-prev!)
+      (editable-text-set! editable (history-data)))
     ((KEY_BACKSPACE)
       (if (= 0 (editable-length editable))
         (command-line-leave editable)
@@ -93,14 +192,17 @@
 (define (command-line-pos)
   (cons (- (LINES) 1) 1))
 
+(define (command-line-init!)
+  (set-input-mode! 'edit-mode *command-line* (command-line-pos))
+  (history-init!)
+  (command-line-changed!))
+
 (define (enter-eval-mode)
   (set! *command-line-mode* 'eval-mode)
-  (set-input-mode! 'edit-mode *command-line* (command-line-pos))
-  (command-line-changed!))
+  (command-line-init!))
 
 (define (enter-search-mode)
   (set! *command-line-mode* 'search-mode)
-  (set-input-mode! 'edit-mode *command-line* (command-line-pos))
-  (command-line-changed!))
+  (command-line-init!))
 
 (define (command-line-mode) *command-line-mode*)
